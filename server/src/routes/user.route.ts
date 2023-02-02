2b4cba6a5a9d1e1e6bef8d434474aca6475ca358
chrisdadev13 <chrisdadev13@gmail.com>
2023-02-01 22:07:29 -0400

    User register and login routes

--- server/src/routes/user.route.ts
+++ server/src/routes/user.route.ts
@@ -1,5 +1,9 @@
 import { Router, Request as Req, Response as Res } from "express";
+import UserController from "../controllers/user.controller";
 
 const userRouter = Router();
+
+userRouter.post("/register", UserController.register);
+userRouter.post("/login", UserController.login);
 
 export default userRouter;
