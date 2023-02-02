bc75423e7d4a06f637a0ac8568ccebe6449016c2
chrisdadev13 <chrisdadev13@gmail.com>
2023-02-01 22:08:17 -0400

    Env constants

--- server/src/configs/constants.ts
+++ server/src/configs/constants.ts
@@ -1,3 +1,5 @@
+import "dotenv/config";
+
 export const PORT = process.env.PORT;
 
 export const DB_ADMIN = process.env.DB_ADMIN;
@@ -5,3 +7,5 @@ export const DB_PASSWORD = process.env.DB_PASSWORD;
 export const DB_NAME = process.env.DB_NAME;
 
 export const DB_URI = `mongodb+srv://${DB_ADMIN}:${DB_PASSWORD}@cordia.ivpwndf.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
+
+export const TOKEN_KEY = process.env.SECRET_TOKEN;
