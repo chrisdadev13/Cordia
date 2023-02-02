0ac2b4f4c111dc1839edd1b9058405bb811a6fb5
chrisdadev13 <chrisdadev13@gmail.com>
2023-02-02 19:49:11 -0400

    Sign up, sign in and get user routes created

--- server/src/routes/user.route.ts
+++ server/src/routes/user.route.ts
@@ -1,9 +1,13 @@
 import { Router, Request as Req, Response as Res } from "express";
 import UserController from "../controllers/user.controller";
+import auth from "../middlewares/auth";
 
 const userRouter = Router();
 
 userRouter.post("/register", UserController.register);
 userRouter.post("/login", UserController.login);
+userRouter.get("/user", auth, (req: any, res: any) => {
+  res.json(req.username);
+});
 
 export default userRouter;
