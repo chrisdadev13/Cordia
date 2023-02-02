81a6100dc6380b7646fc563bc680676310f757a0
chrisdadev13 <chrisdadev13@gmail.com>
2023-02-01 22:07:01 -0400

    User register and login services

--- server/src/services/user.service.ts
+++ server/src/services/user.service.ts
@@ -1,4 +1,52 @@
-export default class UserService{
+import { Request as Req, Response as Res } from "express";
+import { ObjectId } from "mongoose";
+import bcrypt from "bcrypt";
+import UserModel from "../models/user.model";
+import Jwt from "../utils/jwt";
 
+export default class UserService {
+  static async register(
+    firstName: string,
+    lastName: string,
+    username: string,
+    password: string,
+    group: string
+  ) {
+    const doesExist = await UserModel.findOne({ username: username });
+
+    if (doesExist) throw new Error("Username is already taken");
+
+    const encryptedPassword = await bcrypt.hash(password, 10);
+
+    const user = await UserModel.create({
+      firstName: firstName,
+      lastName: lastName,
+      username: username,
+      password: encryptedPassword,
+      group: group,
+    });
+
+    const accessToken = Jwt.signAccess(user._id, user.username);
+
+    await user.save();
+
+    return {
+      user: user,
+      token: accessToken,
+    };
+  }
+
+  static async login(username: string, password: string) {
+    const user = await UserModel.findOne({ username: username });
+    const validPassword = async (password: string, userPassword: string) =>
+      await bcrypt.compare(password, userPassword);
+
+    if (user && validPassword(password, user.password)) {
+      const accessToken = Jwt.signAccess(user._id, user.username);
+      return {
+        user: user,
+        token: accessToken,
+      };
+    }
+  }
 }
-
