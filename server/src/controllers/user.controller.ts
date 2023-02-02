27db46967b7ace65e53dddf4870b1ea0f994810f
chrisdadev13 <chrisdadev13@gmail.com>
2023-02-01 22:07:14 -0400

    User register and login methods

---
+++ server/src/controllers/user.controller.ts
@@ -0,0 +1,25 @@
+import { Request as Req, Response as Res } from "express";
+import UserModel from "../models/user.model";
+import UserService from "../services/user.service";
+
+export default class UserController {
+  static async register(req: Req, res: Res) {
+    const { firstName, lastName, username, password, group } = req.body;
+    const user = await UserService.register(
+      firstName,
+      lastName,
+      username,
+      password,
+      group
+    );
+
+    res.json({ data: user });
+  }
+  static async login(req: Req, res: Res) {
+    const { username, password } = req.body;
+
+    const user = await UserService.login(username, password);
+
+    res.json({ data: user });
+  }
+}
