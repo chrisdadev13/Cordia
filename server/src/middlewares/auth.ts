4a0b4e055a9db7ca08c7145d586bd793e9748f98
chrisdadev13 <chrisdadev13@gmail.com>
2023-02-02 19:48:53 -0400

    Auth middleware

---
+++ server/src/middlewares/auth.ts
@@ -0,0 +1,21 @@
+import { Response as Res, NextFunction as Next } from "express";
+import jwt from "jsonwebtoken";
+import { TOKEN_KEY } from "../configs/constants";
+
+const config = process.env;
+
+export default function auth(req: any, res: Res, next: Next) {
+  const token =
+    req.body.token || req.query.token || req.headers["x-access-token"];
+
+  if (!token)
+    return res.status(403).send("A token is required for authentication");
+
+  try {
+    const decoded = jwt.verify(token, config.TOKEN_KEY);
+    req.user = decoded;
+  } catch (error) {
+    console.log(error);
+  }
+  return next();
+}
