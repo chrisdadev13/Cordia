4c0fd1c01ae33533a88050c14d34bd1ad95bd2a2
chrisdadev13 <chrisdadev13@gmail.com>
2023-02-05 15:57:32 -0400

    Client side - dark mode support

--- client/src/hooks/useTheme.tsx
+++ client/src/hooks/useTheme.tsx
@@ -1,11 +1,29 @@
 import React from "react";
 
-export default function useTheme(){
-  const [darkMode, setDarkMode] = React.useState<boolean>(false);
+export default function useTheme() {
+  const [dark, setDark] = React.useState<boolean>(false);
+  const theme = dark === true ? "dark" : "light";
 
   const toggleTheme = () => {
-    if(darkMode == false){
-      localStorage.
+    setDark(!dark);
+  };
+
+  React.useEffect(() => {
+    const root = window.document.documentElement;
+
+    localStorage.setItem("theme", theme);
+
+    if (localStorage.getItem("theme") === "dark") {
+      setDark(true);
+      root.classList.add(theme);
+    } else {
+      setDark(false);
+      root.classList.remove("dark");
     }
-  }
+  }, [dark, theme]);
+
+  return {
+    dark,
+    toggleTheme,
+  };
 }
