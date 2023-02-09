a25f7dd0000db223121dc839cec2a2a0b02931dc
chrisdadev13 <chrisdadev13@gmail.com>
2023-02-08 22:28:00 -0400

    Server side - send and receive messages using socketio

--- server/src/utils/chat.ts
+++ server/src/utils/chat.ts
@@ -1,25 +1,48 @@
 import { Server } from "socket.io";
 import GroupModel from "../models/group.model";
 
+interface JoinValues {
+  username: string;
+  groupId: string;
+}
+
+interface MessageValues {
+  username: string;
+  groupId: string;
+  message: string;
+}
+
 export default class Chat {
   static connection(app: any) {
-    const io = new Server(app);
+    const io = new Server(app, {
+      cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+      },
+    });
 
     io.on("connection", (socket) => {
       console.log("User connected");
 
-      socket.on("join-room", (groupId) => {
-        GroupModel.findById(groupId, (error: any, chatroom: any) => {
+      socket.on("join-room", (data) => {
+        const { username, invitation } = data;
+        GroupModel.findById(invitation, (error: any, chatroom: any) => {
           if (error) return error;
           if (!chatroom) return "Chatroom not found";
 
-          socket.join(groupId);
-          console.log("User joined room " + groupId);
+          socket.join(invitation);
+          console.log(`${username} joined the room ${invitation}`);
         });
       });
 
-      socket.on("send-message", (groupId, message) => {
-        io.to(groupId).emit("message", message);
+      socket.on("send-message", (data) => {
+        const { username, group, message, time } = data;
+        io.to(group).emit("send-message", {
+          username: username,
+          message: message,
+          time: time,
+        });
+        console.log(data);
       });
 
       socket.on("disconnect", () => {
