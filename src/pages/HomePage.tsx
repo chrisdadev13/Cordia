981767c0488254620ef17cfbd3aa355af70376f6
chrisdadev13 <chrisdadev13@gmail.com>
2023-02-09 20:36:53 -0400

    Client side - minor changes with routing and responsive design

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,18 @@
 import React, { useEffect } from "react";
+import { useNavigate } from "react-router";
 import HomePanel from "../components/Home/HomePanel";
 import useAuth from "../hooks/useAuth";
 
 function HomePage() {
   const { user, userGroups, getUser, getGroups } = useAuth();
+  const navigate = useNavigate();
   useEffect(() => {
-    getUser();
-    getGroups();
+    if (localStorage.getItem("token") !== null) {
+      getUser();
+      getGroups();
+    } else {
+      navigate("/login");
+    }
   }, []);
   return (
     <div className="flex h-screen flex-col items-center justify-center w-screen dark:bg-black dark:text-white">
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -6,8 +6,10 @@ function LandingPage() {
       className="flex
       flex-col items-center justify-center w-screen"
     >
-      <h1 className="font-semibold mb-5">Cordia Chat</h1>
-      <p className="w-1/3 mb-5 font-serif">
+      <h1 className="font-semibold mb-5 xl:text-5xl lg:text-5xl md:text-5xl text-3xl">
+        Cordia Chat
+      </h1>
+      <p className="lg:w-1/3 md:1/3 xl:1/3 mb-5 font-serif">
         Cordia is a minimalist, real-time text groupal, chat app. It&apos;s
         place where you can talk and interact with people like you, make new
         friends, share new thoughts and, most importantly, have fun being
